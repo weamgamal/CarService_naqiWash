f5b16034d8616695948f02e666130cde0b440a33
unknown <weamgamal09@gmail.com>
2023-06-06 13:11:26 +0300

    add

---
+++ CarService/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CarService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("systemIndex");
+        }
+
+        public IActionResult sytemIndex()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CarService/Models/CarServiceEntities.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarService.Models
+{
+    public class CarServiceEntities : IdentityDbContext<ApplicationUser>
+    {
+
+        public CarServiceEntities() : base()
+        {
+
+        }
+        //using during injection
+        public CarServiceEntities(DbContextOptions option) : base(option)
+        {
+
+        }
+
+        public DbSet<Times> Times { get; set; }
+        public DbSet<Technican> Technicans { get; set; }
+        public DbSet<CallCenter> CallCenter { get; set; }
+        public DbSet<CallCenterData> CallCenterData { get; set; }
+        public DbSet<CoordinatorData> CoordinatorData { get; set; }
+        public DbSet<TechnicanData> TechnicanData { get; set; }
+        public DbSet<PreRequst> PreRequst { get; set; }
+        public DbSet<Coordinator> Coordinator { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<Services> Services { get; set; }
+        public DbSet<TechnicalOrderDay> TechnicalOrderDay { get; set; }
+        public DbSet<TechnicalOrderDayTime> TechnicalOrderDayTime { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog = NaqiWashV2 ;Integrated Security = True;  Encrypt = False");
+        }
+    }
+}
